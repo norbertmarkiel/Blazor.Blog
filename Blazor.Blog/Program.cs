7b8578da96d0cd7e0da745e7bb922a60422a1447
Norbert <norbertmarkiel@gmail.com>
2024-06-18 20:03:45 +0200

    InitCommit

---
+++ Blazor.Blog/Program.cs
@@ -0,0 +1,48 @@
+using Blazor.Blog.Components;
+using Data;
+using Data.Models.Interfaces;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+builder.Services.AddOptions<BlogApiJsonDirectAccessSetting>().
+Configure(options =>
+{
+    options.DataPath = @"..\DataInJson\";
+    options.BlogPostsFolder = "Blogposts";
+    options.TagsFolder = "Tags";
+    options.CategoriesFolder = "Categories";
+    options.CommentsFolder = "Comments";
+});
+builder.Services.AddScoped<IBlogApi, BlogApiJsonDirectAccess>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(Blazor.Blog.Client._Imports).Assembly)
+    .AddAdditionalAssemblies(typeof(SharedComponents.Pages.Home).Assembly);
+
+app.Run();
